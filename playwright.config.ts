51a757a7aed8310ff3a8b7fa2291fe4f0b68278b
Ruslan Strazhnyk <strazhnyk@gmail.com>
2025-02-26 21:02:33 +0100

    setting up the env for github workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5173',
+    baseURL: 'http://localhost:3000', // Updated baseURL
     headless: true,
     viewport: { width: 1920, height: 1080 },
     //video: 'retain-on-failure',
