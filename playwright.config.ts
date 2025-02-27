d355652a992162bb03279119a6c3ae97eedf8067
Ruslan Strazhnyk <strazhnyk@gmail.com>
2025-02-27 14:14:53 +0100

    updated readme and config

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,8 +18,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: 0,
-  //retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 3 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -27,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5173', // Updated baseURL
+    baseURL: 'http://localhost:5173',
     headless: true,
     viewport: { width: 1920, height: 1080 },
     //video: 'retain-on-failure',
